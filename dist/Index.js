e4e230bf554c7370cb427c0f9097bf3c2d99af12
Eduardo Dguez <renixedu@gmail.com>
2024-07-18 12:21:04 -0600

    Configuración
    Se configura en el archivo tscofnig.json el compilador y depurador se typescript

---
+++ dist/Index.js
@@ -0,0 +1,6 @@
+"use strict";
+let mensaje = "Hola mundo";
+console.log(mensaje);
+mensaje = "TypeScript";
+console.log(mensaje);
+//# sourceMappingURL=Index.js.map
---
+++ src/Index.ts
@@ -0,0 +1,4 @@
+let mensaje = "Hola mundo";
+console.log(mensaje);
+mensaje = "TypeScript";
+console.log(mensaje);
