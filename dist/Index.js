dadca95f01c2a1cf6fb673712dc33646330418ae
Eduardo Dguez <renixedu@gmail.com>
2024-07-19 12:50:06 -0600

    Tipos avanzados
    Se hacen algunos ejemplos de aprendizaje sobre los tipos de datos avanzados que se puede usar en TypeScript

--- dist/Index.js
+++ dist/Index.js
@@ -23,4 +23,84 @@ var Talla;
 const variable1 = Talla.ExtraGrande;
 console.log(variable1);
 const estado = 2;
+const object = {
+    id: 1,
+    nombre: 'Juan',
+    talla: Talla.Grande,
+    direccion: {
+        numero: 10,
+        calle: "Calle 1",
+        pais: "México"
+    }
+};
+object.nombre = "eduardo";
+object.talla = Talla.Mediana;
+const fn = () => {
+    console.log("función que no retorna nada");
+};
+const fn1 = () => {
+    console.log("función que no retorna cadena");
+    return "";
+};
+const fn2 = () => {
+    let x = 2;
+    if (x > 5) {
+        return 10;
+    }
+    else {
+        return 1;
+    }
+};
+const fn3 = (edad) => {
+    if (edad > 17) {
+        return "Eres mayor";
+    }
+    else {
+        return "No eres mayor";
+    }
+};
+function validaEdad(edad, msg = 'sin nombre') {
+    if (edad > 17)
+        return `some text ${msg}`;
+    return 'No eres mayor de edad';
+}
+validaEdad(18, 'Eduardo');
+function ErrorUsuario() {
+    throw new Error("Error");
+}
+let animal = { id: 1, estado: '', name: '' };
+function sumaDos(n) {
+    if (typeof n == 'number') {
+        return n + 2;
+    }
+    return parseInt(n) + 2;
+}
+sumaDos('2');
+const product = {
+    created_at: '',
+    modified_at: '',
+    name: ''
+};
+const nDeSerie = 3;
+function toNumber(s) {
+    if (!s)
+        return 0;
+    return parseInt(s);
+}
+const n = toNumber(undefined);
+function getUser(id) {
+    if (id < 0) {
+        return null;
+    }
+    return { id: 1, name: 'Eduardo', created_at: new Date() };
+}
+const user = getUser(1);
+console.log('Usuario ', user === null || user === void 0 ? void 0 : user.created_at);
+if (user && user.created_at) {
+    console.log(user.created_at);
+}
+const arra1 = null;
+console.log(arra1 === null || arra1 === void 0 ? void 0 : arra1[0]);
+const fn5 = null;
+console.log(fn5 === null || fn5 === void 0 ? void 0 : fn5());
 //# sourceMappingURL=Index.js.map
--- src/Index.ts
+++ src/Index.ts
@@ -122,5 +122,80 @@ function ErrorUsuario (): never {
 }
 
 
+//Union Types
+type Animal = {
+    id:number,
+    estado:string
+}
+
+type Usuario = {
+    id:number,
+    name:string
+}
+
+let animal: Usuario| Animal = {id:1, estado:'', name:''}
+function sumaDos(n:number| string): number{
+    if(typeof n=='number'){
+        return n+2
+    }
+    return parseInt(n)+2
+}
+
+sumaDos('2')
+
+
+//intersection types
+
+type Audit = {
+    created_at: string,
+    modified_at: string
+}
+
+type Product = {
+    name:string
+}
+
+const product : Audit & Product= {
+    created_at: '',
+    modified_at: '',
+    name: ''
+}
+
+
+//literal types
+type serie = 0|1|2|3|4|5|6|7|8
+const nDeSerie = 3
+
+//nullable types 
+function toNumber (s:string| null|undefined){
+    if (!s) return 0
+    return parseInt(s)
+}
+const n = toNumber(undefined)
+
+
+//Optional types  el signo es ?
+
+function getUser (id:number){
+    if(id<0){return null}
+    return {id:1, name:'Eduardo', created_at:new Date()}
+}
+
+const user = getUser(1)
+console.log('Usuario ', user?.created_at)
+if(user&& user.created_at){
+    console.log(user.created_at)
+}
+const arra1 = null
+console.log(arra1?.[0])
+const fn5:any = null
+console.log(fn5?.())
+
+// type assertions (AS) o (<>) para cuando se conoce el tipo
+const input = document.getElementById('username') as HTMLAnchorElement
+input.innerHTML= "value"
+
+const input1 = <HTMLAnchorElement>document.getElementById('username')  
+input.innerHTML= "value"
 
 
